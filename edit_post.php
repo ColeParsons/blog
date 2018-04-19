a72ae38b787950df1aa3dae0230823cec1996a77
ColeParsons <dparson9@uncc.edu>
2018-04-19 11:17:49 -0700

    Update edit_post.php

--- edit_post.php
+++ edit_post.php
@@ -1,17 +1,12 @@
 <?php
-
 session_start();
 include_once("db.php");
 
 if(!isset($_SESSION['username'])){
-
-
     header("Location:login.php");
     return ;
 }
 if(!isset($_GET['pid'])){
-
-
     header("Location:index.php");
     return ;
 }
@@ -25,21 +20,16 @@ if(isset($_POST['update'])){
 
     $title = mysqli_real_escape_string($db, $title);
     $content = mysqli_real_escape_string($db, $content);
-
     $date = date('1 jS \of F Y h:i:s A');
 
     $sql= "UPDATE  posts SET title='$title', content='$content' ,date='$date' WHERE id=$pid";
 
-
     if($title=="" || $content==""){
         mysqli_query($db, $sql);
-
         header("Location: index66.php");
         return;
-
     }
     mysqli_query($db, $sql);
-
     header("Location: index66.php");
 }
 ?>
@@ -47,9 +37,7 @@ if(isset($_POST['update'])){
 <html>
 <head>
     <link rel="stylesheet" href="StyleSheet55.css" type="text/css" />
-
     <script type="text/javascript" src="C:\xampp\htdocs\blog\blogjs.js"></script>
-
 </head>
 <meta charset="utf-8" />
 <title></title>
@@ -59,9 +47,6 @@ if(isset($_POST['update'])){
 
 <aside id="heading">
     JENIBEEE
-
-
-
 </aside>
 <aside id="glowbar"></aside>
 
@@ -76,35 +61,20 @@ if(isset($_POST['update'])){
             <li><a href="C:\Users\cole\Desktop\Jenibee blog\HTMLPage3.html"> Life Coaching</a></li>
             <li><a href="C:\Users\cole\Desktop\Jenibee blog\HTMLPage3.html"> Contact</a></li>
         </aside>
-
-
-
     </ul>
-
 </div>
 <body>
 <?php
 echo '<figure <a href="AboutJeni.php"><img src="IMG_20171008_181042565_HDR.jpg" class="img2" width="999"  height="360"  /></a></figure>';
-
-
-
-
-
-
 echo'<div class="container">
-        
         <div class="content">
             <a href="" target="_blank">
                 <div class="content-overlay">About JENIBEEE</div>
                 <img class="content-image" src="White Sands 8.jpg" class="img3" width="250" height="360">
-
             </a>
         </div>
     </div>';
-
 ?>
-
-
 
 <aside id="twitter">
     <a class="twitter-timeline" href="https://twitter.com/jenibeeeee?ref_src=twsrc%5Etfw" height="360" width="250">Tweets by jenibeeeee</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
@@ -116,10 +86,6 @@ $res = mysqli_query($db,$sql_get);
 
 if(mysqli_num_rows($res)> 0){
 while($row=mysqli_fetch_assoc($res)){
-
-
-
-
     $title=$row['title'];
     $content=$row['content'];
 
@@ -127,40 +93,28 @@ while($row=mysqli_fetch_assoc($res)){
    echo" <input placeholder='title' name='title' type='text'value='$title' autofocus size='48'<br><br>";
    echo "<textarea placeholder='content' name='content' rows='20' cols='50'>$content</textarea>";
 }
-
-
 }
 ?>
 <form action="edit_post.php" method="post" enctype="multipart/form-data">
     <input placeholder="title" name="title" type="text" autofocus size="48"<br><br>
     <textarea placeholder="content" name="content" rows="20" cols="50"></textarea>
-
     <input name="update" type="submit" value="update">
-
-
-
     <form action = "" method = "POST" enctype = "multipart/form-data">
         <input type = "file" name = "image" />
         <input type = "submit"/>
-
         <ul>
             <li>Sent file: <?php echo $_FILES['image']['name'];  ?>
             <li>File size: <?php echo $_FILES['image']['size'];  ?>
             <li>File type: <?php echo $_FILES['image']['type'] ?>
         </ul>
-
     </form>
-
     <aside id="category">Category</aside>
     <select name="cars">
         <aside id="category">Category:</aside>
         <option value="volvo">Mind</option>
         <option value="saab">Body</option>
         <option value="fiat">Soul</option>
-
     </select>
-
-
 </form>
 </body>
 </html>
